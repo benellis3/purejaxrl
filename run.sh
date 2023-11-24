8853e4827e4d5c44a96e67292cc7a838a2de58f4
benellis3 <benellis@robots.ox.ac.uk>
2023-11-24 11:00:38 +0000

    Add debugging script

---
+++ run.sh
@@ -0,0 +1,14 @@
+WANDB_API_KEY=$(cat $HOME/.oxwhirl_wandb_api_key)
+docker run -it \
+    --gpus device=0 \
+    -e WANDB_API_KEY=$WANDB_API_KEY \
+    -e XLA_PYTHON_CLIENT_PREALLOCATE=false \
+    -e TF_CUDNN_DETERMINISTIC=1 \
+    -e PYTHONPATH=/home/duser/purejaxrl/purejaxrl \
+    -v $(pwd):/home/duser/purejaxrl \
+    --name purejaxrl_${user}_${gpu} \
+    --user $(id -u) \
+    -d \
+    --rm \
+    -t purejaxrl:benlis_brax \
+    /bin/bash 
